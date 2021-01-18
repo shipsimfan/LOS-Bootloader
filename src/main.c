50c41c8baf9f84a33b99d18da7f95cd589906ae4
Lance Hart <shipsimfan@hotmail.com>
2021-01-17 20:48:32 -0500

    Removed efilib.h

--- src/main.c
+++ src/main.c
@@ -1,32 +1,22 @@
 #include <efi.h>
-#include <efilib.h>
 
-EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
-    EFI_STATUS Status;
-    EFI_INPUT_KEY Key;
+EFI_SYSTEM_TABLE* ST;
 
-    /* Store the system table for future use in other functions */
-    ST = SystemTable;
+int Print(CHAR16* str) {
+    EFI_STATUS s = ST->ConOut->OutputString(ST->ConOut, str);
+    return EFI_ERROR(s) ? -1 : 0;
+}
 
-    /* Say hi */
-    Status = ST->ConOut->OutputString(ST->ConOut, L"Hello World\r\n"); // EFI Applications use Unicode and CRLF, a la Windows
-    if (EFI_ERROR(Status))
-        return Status;
+EFI_STATUS efi_main(EFI_HANDLE imageHandle, EFI_SYSTEM_TABLE* systemTable) {
+    ST = systemTable;
 
-    /* Now wait for a keystroke before continuing, otherwise your
-       message will flash off the screen before you see it.
+    // Turn off the watchdog
+    ST->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
 
-       First, we need to empty the console input buffer to flush
-       out any keystrokes entered before this point */
-    Status = ST->ConIn->Reset(ST->ConIn, FALSE);
-    if (EFI_ERROR(Status))
-        return Status;
+    Print(L"Booting . . .\r\n");
 
-    /* Now wait until a key becomes available.  This is a simple
-       polling implementation.  You could try and use the WaitForKey
-       event instead if you like */
-    while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY)
+    while (1)
         ;
 
-    return Status;
+    return EFI_SUCCESS;
 }
